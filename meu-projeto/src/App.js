21ae1422580a9e3eae8a08d4800ed61abe8e2628
Leonardo Araújo <progamadorweb18@gmail.com>
2025-07-16 11:34:40 -0300

    Meu primeiro projeto com React

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
+import HelloWorld from './components/HelloWorld';
+
 
 function App() {
+
+  const nome = "Leonardo";
+  const newName = nome.toLocaleUpperCase();
+
+  function soma(a,b){
+    return a + b;
+  }
+
+  const url = 'https://www.pexels.com/pt-br/foto/filiais-setores-galhos-ramos-16768648/'
+
   return (
-    <div className="App">
-      <h1>Olá, Mundo</h1>
-      <p>Estou aprendendo React</p>
+    <div className='App'>
+      <h2>Alterando o JSX</h2>
+      <p>Olá, {newName}</p>
+      <p>Resultado da soma: {soma(10,5)}</p>
+      <img src={url} alt="Minha imagem"/>
+      <HelloWorld />
     </div>
   );
 }
