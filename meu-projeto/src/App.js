84e866acd24af10d23bfffb680034ee14a6ba43c
Leonardo Araújo <progamadorweb18@gmail.com>
2025-07-16 12:35:06 -0300

    Exercício com React

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import HelloWorld from './components/HelloWorld';
 import SayMyName from './components/SayMyName';
 import Pessoa from './components/Pessoa';
+import Frase from './components/Frase';
 
 
 function App() {
@@ -11,12 +12,13 @@ function App() {
   return (
     <div className='App'>
       <HelloWorld />
+      <Frase />
       <SayMyName nome="Leonardo" />
       <SayMyName nome="Matheus" />
       <SayMyName nome={nome} />
       <Pessoa 
       nome="Daniel" 
-      idade="18" profissao="Progamador" foto="https://via.placeholder.com/150" />
+      idade="18" profissao="Progamador" />
     </div>
   );
 }
--- meu-projeto/src/components/Pessoa.js
+++ meu-projeto/src/components/Pessoa.js
@@ -1,7 +1,6 @@
-function Pessoa({nome, foto, idade, profissao}){
+function Pessoa({nome, idade, profissao}){
     return(
         <div>
-            <img src={foto} alt={nome}/>
             <h2>Nome: {nome}</h2>
             <p>Idade: {idade}</p>
             <p>Profissão: {profissao}</p>
