4763a7286b5fdecb52ed25e3f2d46206888f917e
Leonardo Araújo <progamadorweb18@gmail.com>
2025-07-19 12:33:18 -0300

    Exercício de React

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -4,6 +4,7 @@ import SayMyName from './components/SayMyName';
 import Pessoa from './components/Pessoa';
 import Jogos from './components/Jogos';
 import Frase from './components/Frase';
+import List from './components/List';
 
 function App() {
 
@@ -16,6 +17,11 @@ function App() {
       nome="Leo" 
       idade="19" 
       profissao="Progamador" foto="https://via.placeholder.com/150" />
+      <Pessoa 
+      nome="Rodolfo" 
+      idade="32" 
+      profissao="Eletricista" foto="https://via.placeholder.com/150" />
+      <List />
       <h1>Jogos que eu gosto</h1>
       <Jogos jogo="Free Fire" />
       <Jogos jogo="Honnor of Kings" />
