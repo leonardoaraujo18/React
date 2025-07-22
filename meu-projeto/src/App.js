040198203333a65645cd092b1a594dceefd5ef51
Leonardo Araújo <progamadorweb18@gmail.com>
2025-07-22 14:35:38 -0300

    Exercício com React

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,18 +1,21 @@
-import { useState } from 'react';
-import './App.css';
-import SeuNome from './components/SeuNome';
-import Saudacao from './components/Saudacao';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import Empresa from './pages/Empresa';
+import Contato from './pages/Contato';
+import Navbar from './components/layouts/Navbar';
+import Footer from './components/layouts/Footer';
 
 function App() {
-
-  const [nome, setNome] = useState();
-  
   return (
-    <div className='App'>
-      <h1>State Lift</h1>
-      <SeuNome setNome={setNome}/>
-      <Saudacao nome={nome}/>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/empresa" element={<Empresa />} />
+        <Route path="/contato" element={<Contato />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
